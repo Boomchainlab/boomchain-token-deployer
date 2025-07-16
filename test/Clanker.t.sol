8b5f2197b5b3e8e2f61e500e5ef85809e7067be8
BoomchainLabs <202796817+BoomchainLabs@users.noreply.github.com>
2025-07-16 06:53:04 +0100

    Create Clanker.t.sol
    Signed-off-by: BoomchainLabs  <202796817+BoomchainLabs@users.noreply.github.com>

---
+++ test/Clanker.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import "forge-std/Test.sol";
+import "../contracts/Clanker.sol";
+
+contract ClankerTest is Test {
+    function testDeployment() public {
+        assertTrue(true);
+    }
+}
